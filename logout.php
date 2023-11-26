aa661b72ae1897c2cc39744987123e152245e5e1
Sunit Chandra <sunitorchandra@gmail.com>
2023-11-26 11:47:16 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@
   </head>
   <body>
     <h1>Logged Out</h1>
-    <h2>To Login, <a href="index.php">Click Here</a></h2>
+    <h2>To Login, <a href="ad_index.php">Click Here</a></h2>
   </body>
 </html>
